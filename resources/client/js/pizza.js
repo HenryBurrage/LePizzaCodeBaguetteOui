37af68913c6d7bf193f670a22fcd84970a2d6b0b
HenryBurrage <91697@farnborough.ac.uk>
2021-01-20 17:03:03 +0000

    Added button to index page that loads the login page and a logo

--- resources/client/js/pizza.js
+++ resources/client/js/pizza.js
@@ -6,10 +6,10 @@ function getImageData(img) {
 
 function getToppingsList(element) {
     console.log(element.toString());
-    if (element.style.filter == "grayscale(100%)") {
-        (element).style.filter = "grayscale(0%)";
-    } else {
+    if (element.style.filter == "grayscale(0%)") {
         (element).style.filter = "grayscale(100%)";
+    } else {
+        (element).style.filter = "grayscale(0%)";
     }
     console.log("Invoked ToppingsList()");     //console.log your BFF for debugging client side - also use debugger statement
     const url = "/toppings/list/";    		// API method on web server will be in Users class, method list
@@ -37,82 +37,10 @@ function formatToppingsList(myJSONArray){
     document.getElementById("ToppingsTable").innerHTML = dataHTML;
 }
 
-function toggleHam(){
-    if (document.getElementById("hampic").style.visibility === "hidden"){
-        document.getElementById("hampic").style.visibility = "visible";
+function toggle(element){
+    if (element.style.visibility === "hidden"){
+        element.style.visibility = "visible";
     } else {
-        document.getElementById("hampic").style.visibility = "hidden";
-    }
-}
-
-function toggleChicken(){
-    if (document.getElementById("chickenpic").style.visibility === "hidden"){
-        document.getElementById("chickenpic").style.visibility = "visible";
-    } else {
-        document.getElementById("chickenpic").style.visibility = "hidden";
-    }
-}
-
-function toggleBacon(){
-    if (document.getElementById("baconpic").style.visibility === "hidden"){
-        document.getElementById("baconpic").style.visibility = "visible";
-    } else {
-        document.getElementById("baconpic").style.visibility = "hidden";
-    }
-}
-
-function togglePepperoni(){
-    if (document.getElementById("pepperonipic").style.visibility === "hidden"){
-        document.getElementById("pepperonipic").style.visibility = "visible";
-    } else {
-        document.getElementById("pepperonipic").style.visibility = "hidden";
-    }
-}
-
-function toggleChorizo(){
-    if (document.getElementById("chorizopic").style.visibility === "hidden"){
-        document.getElementById("chorizopic").style.visibility = "visible";
-    } else {
-        document.getElementById("chorizopic").style.visibility = "hidden";
-    }
-}
-
-function toggleOlive(){
-    if (document.getElementById("olivepic").style.visibility === "hidden"){
-        document.getElementById("olivepic").style.visibility = "visible";
-    } else {
-        document.getElementById("olivepic").style.visibility = "hidden";
-    }
-}
-
-function toggleOnion(){
-    if (document.getElementById("onionpic").style.visibility === "hidden"){
-        document.getElementById("onionpic").style.visibility = "visible";
-    } else {
-        document.getElementById("onionpic").style.visibility = "hidden";
-    }
-}
-
-function togglePepper(){
-    if (document.getElementById("pepperpic").style.visibility === "hidden"){
-        document.getElementById("pepperpic").style.visibility = "visible";
-    } else {
-        document.getElementById("pepperpic").style.visibility = "hidden";
-    }
-}
-
-function togglePineapple(){
-    if (document.getElementById("pineapplepic").style.visibility === "hidden"){
-        document.getElementById("pineapplepic").style.visibility = "visible";
-    } else {
-        document.getElementById("pineapplepic").style.visibility = "hidden";
-    }
-}
-
-function toggleTomato(){
-    if (document.getElementById("tomatopic").style.visibility === "hidden"){
-        document.getElementById("tomatopic").style.visibility = "visible";
-    } else {
-        document.getElementById("tomatopic").style.visibility = "hidden";
+        element.style.visibility = "hidden";
     }
 }
